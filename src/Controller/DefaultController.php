703947951705ae6419c18d2f24a3a6dc174b96ba
Bullshitsoftware <pauzern@bullshitsoftware.online>
2021-09-26 23:15:40 +0000

    Initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    #[Route('/', name: 'default')]
+    public function index(): Response
+    {
+        return new Response("test");
+    }
+}
