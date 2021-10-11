3d81749741833cbd86a0acb3cef548ab6883c0f6
Bullshitsoftware <pauzern@bullshitsoftware.online>
2021-10-11 05:28:58 +0000

    Add flash type

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -7,4 +7,5 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 abstract class Controller extends AbstractController
 {
     public const FLASH_SUCCESS = 'flash_success';
+    public const FLASH_ERROR = 'flash_error';
 }
