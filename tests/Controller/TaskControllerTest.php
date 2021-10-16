e3e6499102c5eeed1a4dce207e123575c40db6d9
Bullshitsoftware <pauzern@bullshitsoftware.online>
2021-10-16 08:34:16 +0000

    Add task title to grid, update markup and tests

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -459,7 +459,16 @@ class TaskControllerTest extends WebTestCase
         $this->client->loginUser($this->userRepository->findOneByEmail('john.doe@example.com'));
         $this->client->request('GET', '/view/'.$task->getId());
         self::assertResponseIsSuccessful();
-        self::assertSelectorTextContains('h1', $task->getTitle());
+        self::assertSelectorTextContains('h1', 'Current task 1');
+        self::assertSelectorTextContains('.grid__cell-id', $task->getId()->toRfc4122());
+        self::assertSelectorTextContains('.grid_cell-title', 'Current task 1');
+        self::assertSelectorTextContains('.grid__cell-tag', 'bar foo');
+        self::assertSelectorTextContains('.grid__cell-description', '');
+        self::assertSelectorTextContains('.grid__cell-created', '2007-01-02 02:55:05');
+        self::assertSelectorTextContains('.grid__cell-updated', '2007-01-02 02:55:05');
+        self::assertSelectorTextContains('.grid__cell-wait', '—');
+        self::assertSelectorTextContains('.grid__cell-started', '2007-01-02 02:55:05');
+        self::assertSelectorTextContains('.grid__cell-ended', '—');
     }
 
     public function testEdit(): void
