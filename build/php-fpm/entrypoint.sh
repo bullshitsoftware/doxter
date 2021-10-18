b15973b56a3ce485106e10a8e2aedd67a556891e
Bullshitsoftware <pauzern@bullshitsoftware.online>
2021-10-18 13:04:39 +0000

    Make db backup before migrations run

--- build/php-fpm/entrypoint.sh
+++ build/php-fpm/entrypoint.sh
@@ -2,6 +2,7 @@
 
 php8 /app/bin/console cache:warmup
 php8 /app/bin/console doctrine:database:create
+cp /db/data.db /db/data.db.bak
 php8 /app/bin/console doctrine:migrations:migrate --no-interaction
 
 exec php-fpm8 -F -O
